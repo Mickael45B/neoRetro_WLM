fece78b0b3868f905fad5981d836e8be593df41d
Mickael45B <154462664+Mickael45B@users.noreply.github.com>
2024-02-17 23:03:34 +0100

    Add files via upload

---
+++ 03_discution/Vue_partie03.php
@@ -0,0 +1,236 @@
+<?php
+  /*
+  - Nom du fichier : Vue_partie03.php 
+  - Type : vue
+  - Language(s) : HTML / PHP
+
+  LES ATTRIBUTS/VALEURS DES BALISES SERONT ORGANISE DE LA FACON SUIVANTE :
+    <... id="" class="" data-...="" value="" name="" style="" --DIVERS-- >
+
+  */
+?>        
+
+  <!-- PARTIE 01 ==> --------------------------------------------------------------------------------------------------------------------------------------------------------------------- CORP ==> -->
+    <div id="" class="Discution" data-vide="" value="" name="" style="display:none;">
+      <div id="" class="P3_containerprinc" data-vide="" value="" name="" >
+        <div id="P3_msn-messenger-window" class="P3_msn-messenger-window" data-vide="" value="" name="" >
+          <div id="P3_shakeButton" class="P3_container" data-vide="" value="" name="" >
+
+          <!-- PARTIE 01 01 ==> -------------------------------------------------------------------------------------------------------------------------------------------------- MENU GENERAL ==> -->
+            <div id="" class="P3_msn-messenger-toolbar" data-vide="" value="" name="" >
+              <div id="" class="P3_container" data-vide="" value="" name="" >
+                <div id="" class="P3_toolbar-container" data-vide="" value="" name="" >
+                  <div id="" class="P3_image-circular-button P3_up-down" data-vide="" value="" name="" >
+                    <div id="" class="P3_container" data-vide="" value="" name="" >
+                      <img id="" class="" data-vide="" value="" name=""  src="../bibliotheque/03_discution/icones/small-up-down.png" alt="up/down">
+                    </div>
+                  </div>
+                  <div id="" class="P3_image-button P3_invite" data-vide="" value="" name="" >
+                      <div id="" class="P3_container" data-vide="" value="" name="" >
+                          <a id="" class="" data-vide="" value="" name=""  href="https://discord.com/" target="_blank">
+                              <img id="" class="" data-vide="" value="" name=""  src="../bibliotheque/03_discution/icones/invite.png" alt="Invite">
+                              <div id="" class="P3_text" data-vide="" value="" name="">inviter</div>
+                          </a>
+                      </div>
+                  </div>
+                  <div id="" class="P3_image-button P3_send" data-vide="" value="" name="" >
+                    <div id="" class="P3_container" data-vide="" value="" name="" >
+                      <a id="" class="" data-vide="" value="" name=""  href="https://www.pinterest.fr/" target="_blank">
+                          <img id="" class="" data-vide="" value="" name=""  src="../bibliotheque/03_discution/icones/send.png" alt="envoyer">
+                          <div id="" class="P3_text" data-vide="" value="" name="" >envoyer</div>
+                      </a>
+                    </div>
+                  </div> 
+                  <div id="" class="P3_image-button P3_video" data-vide="" value="" name="">
+                    <div id="" class="P3_container" data-vide="" value="" name="">
+                      <a id="" class="" data-vide="" value="" name="" href="https://www.youtube.com/" target="_blank">
+                          <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/video.png" alt="vidéo">
+                          <div id="" class="P3_text" data-vide="" value="" name="">video</div>
+                      </a>
+                    </div>
+                  </div>
+                  <div id="" class="P3_image-button P3_voice" data-vide="" value="" name="">
+                    <div id="" class="P3_container" data-vide="" value="" name="">
+                      <a id="" class="" data-vide="" value="" name="" href="https://www.teamspeak.com/fr/" target="_blank">
+                          <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/voice.png" alt="parler">
+                          <div id="" class="P3_text" data-vide="" value="" name="">parler</div>
+                      </a>
+                    </div>
+                  </div>
+                  <div id="" class="P3_image-button P3_activities" data-vide="" value="" name="">
+                    <div id="" class="P3_container" data-vide="" value="" name="">
+                      <a id="" class="" data-vide="" value="" name="" href="https://www.deezer.com/fr/" target="_blank">
+                        <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/activities.png" alt="activités">
+                        <div id="" class="P3_text" data-vide="" value="" name="">activités</div>
+                      </a>
+                    </div>
+                  </div>
+                  <div id="" class="P3_image-button P3_games" data-vide="" value="" name="">
+                    <div id="" class="P3_container" data-vide="" value="" name="">
+                      <a id="" class="" data-vide="" value="" name="" href="https://gaming.amazon.com/intro" target="_blank">
+                        <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/games.png" alt="jeux">
+                        <div id="" class="P3_text" data-vide="" value="" name="">jeux</div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div id="" class="P3_toolbar-small-container" data-vide="" value="" name="">
+                  <div id="" class="P3_left" data-vide="" value="" name=""></div>
+                  <div id="" class="P3_center" data-vide="" value="" name="">
+                    <div id="" class="P3_buttons" data-vide="" value="" name="">
+                      <div id="" class="P3_image-circular-button P3_block" data-vide="" value="" name="">
+                        <div id="" class="P3_container" data-vide="" value="" name="">
+                          <img id="DiscutionButton" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/small-block.png" alt="bloquer">
+                        </div>
+                      </div>
+                      <div  id="" class="P3_image-circular-button P3_paint" data-vide="" value="" name="">
+                        <div id="" class="P3_container" data-vide="" value="" name="">
+                          <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/small-paint.png" alt="dessin">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div id="" class="P3_right" data-vide="" value="" name=""></div>
+                  <div id="" class="P3_end" data-vide="" value="" name="">
+                    <img id="quitter" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/allumer.png" style="height: 15px;">
+                  </div>
+                </div>
+              </div>
+            </div>
+          <!-- <== PARTIE 01 01 -------------------------------------------------------------------------------------------------------------------------------------------------- <== MENU GENERAL -->
+
+          <!-- PARTIE 01 02 ==> ----------------------------------------------------------------------------------------------------------------------------- DESTINATAIRE / HISTORIQUE MESSAGE ==> -->
+            <div id="" class="P3_msn-messenger-remote-user" data-vide="" value="" name="">
+              <div id="" class="P3_container" data-vide="" value="" name="">
+                <div id="" class="P3_msn-messenger-history-chat" data-vide="" value="" name="">
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <div id="" class="P3_subject" data-vide="" value="" name=""></div>
+                    <div id="historiqueMess" class="" data-vide="" value="" name="" style="justify-content:left; ">
+                      <div id="historiqueMessageRecu" class="" data-vide="" value="" name="" rows="8" cols="30" style="width: 100%; height:100%; border:none; resize: none; ">
+                        <div id="messageHistory" class="" data-vide="" value="" name=""></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div id="contenerAvatarDestinataire" class="P3_msn-messenger-avatar P3_image" data-vide="" value="" name="">
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="avatarDiscutionDestinataire" class="P3_picture P3_pictureDestinataire" data-vide="" value="" name="" src="../00_general/avatars/msn.png" alt="Avatar">
+                  </div>
+                </div>
+              </div>
+            </div>
+          <!-- <== PARTIE 01 02 ----------------------------------------------------------------------------------------------------------------------------- <== DESTINATAIRE / HISTORIQUE MESSAGE -->
+
+          <!-- PARTIE 01 03 ==> --------------------------------------------------------------------------------------------------------------------------------------------------- UTILISATEUR ==> -->
+            <div id="" class="P3_msn-messenger-local-user" data-vide="" value="" name="">
+              <div id="" class="P3_actionbar" data-vide="" value="" name="">
+                <div id="" class="P3_simple-button P3_letter" data-vide="" value="" name="">
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/letter.png" alt="ecrire">
+                    <span></span>
+                  </div>
+                </div>
+                <div id="" class="P3_simple-button happy" data-vide="" value="" name="">
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/happy.png" alt="smiley">
+                    <span></span>
+                    <button id="" class="P3_downEmoji" data-vide="" value="" name="">⯆</button>
+                  </div>
+                </div>
+                <div id="" class="P3_simple-button P3_voice-clip" data-vide="" value="" name="" label="Voice clip">
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/voice-clip.png" alt="son">
+                  </div>
+                </div>
+                <div id="blagueChuckNorris" class="P3_simple-button P3_wink" data-vide="" value="" name="">
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="" class="" data-vide="" value="" name="" src="emojipng.com-6357000.png" alt="smiley2" style="width:16px; height:16px;">
+                    <span></span>
+                  </div>
+                </div>
+                <div id="" class="P3_simple-button P3_mountain" data-vide="" value="" name="" arrow>
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/mountain.png" alt="photo">
+                    <span></span>
+                    <button id="" class="P3_down" data-vide="" value="" name="">⯆</button>
+                  </div>
+                </div>
+                <div id="" class="P3_simple-button P3_gift" data-vide="" value="" name="" arrow>
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/gift.png" alt="cadeaux">
+                    <span></span>
+                    <button id="" class="P3_downgift" data-vide="" value="" name="">⯆</button>
+                  </div>
+                </div>
+
+                <div id="P3_wizz" class="P3_simple-button P3_nudge" data-vide="" value="" name="">
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/nudge.png" alt="wizz"><audio id="shakeSound" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/son/wizz.wav"></audio>
+                    <span></span>
+                  </div>
+                </div>
+
+              </div>
+              <div id="" class="P3_chat" data-vide="" value="" name="">
+                <div id="classmessageAEnvoye" class="" data-vide="" value="" name="" style="justify-content:left; ">
+                  <textarea id="messageAEnvoye" class="" data-vide="" value="" name="" rows="3" cols="30" style="width: 100%; height:100%; border:none;"></textarea>
+                </div>
+                <div id="" class="P3_buttons P3_buttons2" data-vide="" value="" name="">
+                  <button id="envoyerMessage" class="P3_normal" data-vide="" value="" name="">Send</button>
+                </div>
+              </div>
+              <div id="" class="P3_tabs" data-vide="" value="" name="">
+                <div id="" class="P3_tab-button P3_paint" data-vide="" value="" name="">
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/paint.png" alt="dessin">
+                  </div>
+                </div>
+                <div id="" class="P3_tab-button P3_letter" data-vide="" value="" name="">
+                  <div id="" class="P3_container" data-vide="" value="" name="">
+                    <img id="" class="" data-vide="" value="" name="" src="../bibliotheque/03_discution/icones/letter.png" alt="lettre">
+                  </div>
+                </div>
+              </div>
+              <div id="contenerAvatarUtilisateur" class="P3_msn-messenger-avatar P3_duck" data-vide="" value="" name="">
+                <div id="" class="P3_container" data-vide="" value="" name="">
+                  <img id="avatarDiscutionUtilisateur" class="P3_picture" data-vide="" value="" name="" src="../00_general/avatars/msn.png" alt="Avatar">
+                </div>
+              </div> 
+            </div>
+          <!-- <== PARTIE 01 03 --------------------------------------------------------------------------------------------------------------------------------------------------- <== UTILISATEUR -->
+
+          <!-- PARTIE 01 04 ==> -------------------------------------------------------------------------------------------------------------------------------------------------- PIED DE PAGE ==> -->
+            <div id="" class="P3_msn-messenger-statusbar" data-vide="" value="" name="">
+              <div id="" class="P3_container" data-vide="" value="" name="">
+                <div id="" class="P3_text" data-vide="" value="" name=""></div>
+              </div>
+            </div>
+          <!-- <== PARTIE 01 04 -------------------------------------------------------------------------------------------------------------------------------------------------- <== PIED DE PAGE -->
+
+          </div>
+          <div id="" class="P3_border-window" data-vide="" value="" name=""></div>
+        </div>
+      </div>
+    </div>
+  <!-- <== PARTIE 01 --------------------------------------------------------------------------------------------------------------------------------------------------------------------- <== CORP -->
+
+  <!-- PARTIE 02 ==> -------------------------------------------------------------------------------------------------------------------------------------------------------------------- LIENS ==> -->
+      <!-- API -->
+        <script src="../03_discution/API/API_GIF.js"></script><!-- API pour le choix des GIF -->
+        <script src="../03_discution/API/api_chuckNorris.js"></script><!-- API d'envoi de memes sur CHuck Norris -->
+
+      <!-- fonctionnement -->
+        <script src="../bibliotheque/03_discution/emoji-en-US.json"></script><!-- bibliothe interne de selection/envoi d'emoji -->
+        <script src="../03_discution/fonctionnement/emoji-en-US.js"></script><!-- bibliothe interne de selection/envoi d'emoji -->
+        <script src="../03_discution/fonctionnement/script_discution.js"></script><!-- divers scripts utile a cette page -->
+      
+
+      <!-- Dynamique -->
+        <script src="../03_discution/fonctionnement/script_historiqueMessages.js"></script><!-- divers scripts utile a cette page -->
+
+      <!-- Style --> 
+        <link rel="stylesheet" type="text/css" href="03_discution/styles/style_discution.css"><!-- feuille de style utilise pour cette page -->
+
+      <!-- Animation -->
+        <!-- Pas de fichier(s) pour cette partie -->
+  <!-- <== PARTIE 02 -------------------------------------------------------------------------------------------------------------------------------------------------------------------- <== LIENS -->
